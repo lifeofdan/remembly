4bf5b015a82ebf0c72369d478bb7ea14d3ea42d3
Daniel Newman <contact@lifeofdan.me>
2024-09-19 11:12:42 -0400

    Initial commit

---
+++ lib/remembly/application.ex
@@ -0,0 +1,36 @@
+defmodule Remembly.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      RememblyWeb.Telemetry,
+      Remembly.Repo,
+      {DNSCluster, query: Application.get_env(:remembly, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: Remembly.PubSub},
+      # Start the Finch HTTP client for sending emails
+      {Finch, name: Remembly.Finch},
+      # Start a worker by calling: Remembly.Worker.start_link(arg)
+      # {Remembly.Worker, arg},
+      # Start to serve requests, typically the last entry
+      RememblyWeb.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Remembly.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    RememblyWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
---
+++ test/remembly_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule RememblyWeb.PageControllerTest do
+  use RememblyWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
