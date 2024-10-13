59948907ea940a1de01e645b93675fdf38043386
Daniel Newman <contact@lifeofdan.me>
2024-10-12 23:41:42 -0400

    Create seed data for use in dev

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+Remembly.Remember.message!(%{content: "This is a cool [link]"})
+Remembly.Remember.message!(%{content: "Article about crazy stuff [link]"})
+Remembly.Remember.message!(%{content: "YouTube video [link]"})
+Remembly.Remember.message!(%{content: "Some tutorial I need the next time I'm doing xyz [link]"})
+Remembly.Remember.message!(%{content: "My kid's birthday video [link]"})
+Remembly.Remember.message!(%{content: "Storm footage [link]"})
