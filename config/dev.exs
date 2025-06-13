4a69f6569bca41c2db793a8b70b421503d3903b8
Daniel Newman <contact@lifeofdan.me>
2025-06-13 15:24:18 +0000

    Configure local postgres environment

--- config/dev.exs
+++ config/dev.exs
@@ -2,9 +2,9 @@ import Config
 
 # Configure your database
 config :remembly, Remembly.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
+  username: "dbuser",
+  password: "dbpassword",
+  hostname: "postgres",
   database: "remembly_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
