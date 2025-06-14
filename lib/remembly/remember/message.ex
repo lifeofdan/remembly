d13d6bcf8f1660626ec9e59b054d3ea9c746de01
Daniel Newman <contact@lifeofdan.me>
2025-06-14 21:03:42 +0000

    Add message to category relationship

--- lib/remembly/remember/message.ex
+++ lib/remembly/remember/message.ex
@@ -20,7 +20,7 @@ defmodule Remembly.Remember.Message do
     defaults [:read, :destroy, create: :*, update: :*]
 
     create :remember_message do
-      accept [:content]
+      accept [:reference_id, :content]
     end
   end
 
@@ -28,12 +28,21 @@ defmodule Remembly.Remember.Message do
     uuid_primary_key :id
 
     attribute :content, :string, allow_nil?: false, public?: true
+    attribute :reference_id, :string, allow_nil?: false
 
     timestamps(public?: true)
+  end
+
+  relationships do
+    belongs_to :category, Remembly.Remember.Category, public?: true
   end
 
   calculations do
     calculate :short_content, :string, Remembly.Remember.Message.Calculations.ShortContent,
       allow_nil?: false
+  end
+
+  identities do
+    identity :message_reference_id, [:reference_id]
   end
 end
