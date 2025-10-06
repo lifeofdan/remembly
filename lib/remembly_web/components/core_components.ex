e8c906a5d3dc78d06ec4a87e62b32a58f9607eea
Daniel Newman <contact@lifeofdan.me>
2025-10-06 04:48:28 +0000

    Style core components

--- lib/remembly_web/components/core_components.ex
+++ lib/remembly_web/components/core_components.ex
@@ -337,7 +337,7 @@ defmodule RememblyWeb.CoreComponents do
       <select
         id={@id}
         name={@name}
-        class="mt-2 block w-full rounded-md border border-gray-300 bg-white shadow-sm focus:border-zinc-400 focus:ring-0 sm:text-sm"
+        class="input input-bordered input-secondary w-full max-w-xs self-end"
         multiple={@multiple}
         {@rest}
       >
@@ -379,7 +379,7 @@ defmodule RememblyWeb.CoreComponents do
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
+          "input input-bordered input-secondary border-secondary w-full max-w-xs self-end",
           @errors == [] && "border-zinc-300 focus:border-zinc-400",
           @errors != [] && "border-rose-400 focus:border-rose-400"
         ]}
